cdef5e81cda2d47e18f066d5c9bbbcd7b87a4cd0
Piskieee <piskieee@gmail.com>
2021-06-18 16:43:54 -0700

    Initilize Servo Controls
    Setup based on https://github.com/ZIOCC/Qwiic_Servo_Driver_PCA9685

--- CandySorter.ino
+++ CandySorter.ino
@@ -1,23 +1,36 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <SparkFun_BH1749NUC_Arduino_Library.h>
 
+#define SERVOMIN  150;
+#define SERVOMAX  600;
+
+Adafruit_PWMServoDriver servoDropper = Adafruit_PWMServoDriver();
+Adafruit_PWMServoDriver servoSlide = Adafruit_PWMServoDriver();
 int photoInturruptSignal = 3;
-int drop;
+int slotServo = 0;
+int val;
 
 void setup() {
   // candy detector
   pinMode(photoInturruptSignal, INPUT); //low is triggered
-  
+
+  // servos
+  servoDropper.begin();
+  servoDropper.setPWMFreq(60);
+  servoSlide.begin();
+  servoSlide.setPWMFreq(60);
+
   // enable debug output
   Serial.begin(9600);
+  Serial.println("====Candy Sorter Initialized====");
 }
 
 void loop() {
-  drop = digitalRead(photoInturruptSignal);
+  val = digitalRead(photoInturruptSignal);
 
-  if(drop == LOW) // beam inturrupt
+  if(val == LOW) // beam inturrupt
   {
     Serial.println("dropped");
-    delay(100);
+    delay(200);
   }
 }
