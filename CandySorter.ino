e72a5358c5f64e9a0938979a0386ed14a51911ea
Piskieee <piskieee@gmail.com>
2021-06-18 14:39:59 -0700

    Serial input back

--- CandySorter.ino
+++ CandySorter.ino
@@ -1,12 +1,25 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <SparkFun_BH1749NUC_Arduino_Library.h>
 
-void setup() {
-  // put your setup code here, to run once:
+int photoInturruptSignal = 3;
+int drop;
 
+void setup() {
+  // candy detector
+  pinMode(photoInturruptSignal, INPUT);
+  
+  // enable debug output
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  drop = digitalRead(photoInturruptSignal);
+  if (drop == LOW)
+  {
+    Serial.println("Low");
+  }
+  if (drop == HIGH)
+  {
+    Serial.println("High");
+  }
 }
