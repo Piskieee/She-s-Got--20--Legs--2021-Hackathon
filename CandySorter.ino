8157248345280ca71f7e613dbcc09c4231ee6467
Piskieee <piskieee@gmail.com>
2021-06-18 17:50:56 -0700

    Driving 2 controllers based on photosensor

--- CandySorter.ino
+++ CandySorter.ino
@@ -1,13 +1,12 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <SparkFun_BH1749NUC_Arduino_Library.h>
 
-#define SERVOMIN  150;
-#define SERVOMAX  600;
-
-Adafruit_PWMServoDriver servoDropper = Adafruit_PWMServoDriver();
-Adafruit_PWMServoDriver servoSlide = Adafruit_PWMServoDriver();
+Adafruit_PWMServoDriver servoController = Adafruit_PWMServoDriver();
+#define SERVOMIN  120
+#define SERVOMAX  720
 int photoInturruptSignal = 3;
 int slotServo = 0;
+int slideServo = 1;
 int val;
 
 void setup() {
@@ -15,10 +14,8 @@ void setup() {
   pinMode(photoInturruptSignal, INPUT); //low is triggered
 
   // servos
-  servoDropper.begin();
-  servoDropper.setPWMFreq(60);
-  servoSlide.begin();
-  servoSlide.setPWMFreq(60);
+  servoController.begin();
+  servoController.setPWMFreq(60);
 
   // enable debug output
   Serial.begin(9600);
@@ -32,5 +29,17 @@ void loop() {
   {
     Serial.println("dropped");
     delay(200);
+
+    for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+      servoController.setPWM(slotServo, 0, pulselen);
+      servoController.setPWM(slideServo, 0, pulselen);
+    }
+
+    delay(500);
+    for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+      servoController.setPWM(slotServo, 0, pulselen);
+      servoController.setPWM(slideServo, 0, pulselen);
+    }
+    delay(500);
   }
 }
